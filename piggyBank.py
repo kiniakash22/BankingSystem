2d2a2c3e142696500642561f5287b82355594270
Akash Kini <kiniakash22@gmail.com>
2019-12-15 19:46:42 +0530

    Initial commit with bare basic functionality

---
+++ piggyBank.py
@@ -0,0 +1,39 @@
+balance = 0
+last_transaction = 0
+
+def show_statement():
+    print("Current Balance:", balance)
+    print("Last Tansaction:", last_transaction)
+
+def deposit(amount):
+    global balance, last_transaction
+    balance += amount
+    last_transaction = amount
+    show_statement()
+    return True
+    
+def withdraw(amount):
+    global balance, last_transaction
+    if amount > balance:
+        return False
+    balance -= amount
+    last_transaction = -amount
+    show_statement()
+    return True
+    
+print("Welcome to PiggyBank!")
+
+while True:
+    print("Press:\n1. Check Statement\n2. Deposit Money\n3. Withdraw Money")
+    choice = int(input())
+
+    if choice == 1:
+        show_statement()
+    elif choice == 2:
+        print("Enter the amount you want to desposit:")
+        amount = float(input())
+        done = deposit(amount)
+    elif choice == 3:
+        print("Enter the amount you want to withdraw:")
+        amount = float(input())
+        done = withdraw(amount)
